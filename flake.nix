872976e92b118d92eac0a65ee0bb0a5a412c6ed6
Kishan S Patel <bishan.batel@protonmail.com>
2024-09-17 17:48:02 -0700

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { 
+    self, nixpkgs, flake-utils, ...
+    }: flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell.override {
+        stdenv = pkgs.gcc11Stdenv;
+      } {
+          packages = with pkgs; [
+            pkg-config
+            ninja
+            cmake
+            python3
+            valgrind
+            gcc8
+            xxd
+          ];
+
+          buildInputs = with pkgs; [
+            llvmPackages.clang-tools
+          ];
+        };
+    });
+}
